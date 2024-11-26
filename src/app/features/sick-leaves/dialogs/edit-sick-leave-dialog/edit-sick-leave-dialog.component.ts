2c6b2dc604d20450c858e28c2a247ec6353979a6
VladSimeonidi <simeonidiv@gmail.com>
2024-11-26 19:12:14 +0000

    [T-8] Create dialog component for edit leave

---
+++ src/app/features/sick-leaves/dialogs/edit-sick-leave-dialog/edit-sick-leave-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-sick-leave-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-sick-leave-dialog.component.html',
+  styleUrl: './edit-sick-leave-dialog.component.scss'
+})
+export class EditSickLeaveDialogComponent {
+
+}

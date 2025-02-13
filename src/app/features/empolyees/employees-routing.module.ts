926295213de995417158da51cc728d03bc65c154
VladSimeonidi <simeonidiv@gmail.com>
2025-02-13 22:53:50 +0000

    [T-30] created employee summary component

--- src/app/features/empolyees/employees-routing.module.ts
+++ src/app/features/empolyees/employees-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employees.component';
+import { EmployeeSummaryComponent } from './employee-summary/employee-summary.component';
 
-const routes: Routes = [{ path: '', component: EmployeesComponent }];
+const routes: Routes = [
+    { path: '', component: EmployeesComponent },
+    { path: 'employee-summary/:id', component: EmployeeSummaryComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
